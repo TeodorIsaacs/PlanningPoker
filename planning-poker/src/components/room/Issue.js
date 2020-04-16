7b7c2ba9b627cc7eb5de05fa260ae730d4a3e127
teodorhurtigh <teodor.hurtigh@gmail.com>
2020-04-16 15:03:21 +0200

    [BugFix] Undo stupid link fix

--- planning-poker/src/components/room/Issue.js
+++ planning-poker/src/components/room/Issue.js
@@ -4,19 +4,14 @@ import "./Issue.css"
 
 export default function Issue(props) {
 
-
-    /*
-    This is extremely hacky and I hate it,
-    buuuuut it kinda works, and I have not found a simpler solution online
-     */
     return (
         <div className="issueCard">
-            {props.link && '<a className="ghostLink" target="_blank" href={props.link}>'}
-            <div className="issueContent">
-                <h3 className="issueName">{props.name}</h3>
-                <p className="issueDescription">{props.description}</p>
-            </div>
-            {props.link && '</a> '}
+            <a className="ghostLink" target="_blank" href={props.link}>
+                <div className="issueContent">
+                    <h3 className="issueName">{props.name}</h3>
+                    <p className="issueDescription">{props.description}</p>
+                </div>
+            </a>
         </div>
     )
 }
