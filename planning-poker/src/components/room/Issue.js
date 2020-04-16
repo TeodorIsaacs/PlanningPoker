6fe6fe79af28b69e2a6cbd26de4c4df5415ea41d
teodorhurtigh <teodor.hurtigh@gmail.com>
2020-04-16 03:06:30 +0200

    [BugFix] Make Issue not clickable without link

--- planning-poker/src/components/room/Issue.js
+++ planning-poker/src/components/room/Issue.js
@@ -1,19 +1,22 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import "./Issue.css"
-import {useHistory} from "react-router-dom";
 
 export default function Issue(props) {
 
 
+    /*
+    This is extremely hacky and I hate it,
+    buuuuut it kinda works, and I have not found a simpler solution online
+     */
     return (
         <div className="issueCard">
-            <a className="ghostLink" target="_blank" href={props.link}>
-                <div className="issueContent">
-                    <h3 className="issueName">{props.name}</h3>
-                    <p className="issueDescription">{props.description}</p>
-                </div>
-            </a>
+            {props.link && '<a className="ghostLink" target="_blank" href={props.link}>'}
+            <div className="issueContent">
+                <h3 className="issueName">{props.name}</h3>
+                <p className="issueDescription">{props.description}</p>
+            </div>
+            {props.link && '</a> '}
         </div>
     )
 }
